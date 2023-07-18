0e08ac939184c77ce6d1888463139b357dfac6e3
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-07-18 22:40:13 +0530

    Issue #ULP-000 feat: Removed unused code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,69 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { QuarModule } from '@altack/quar';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
-import * as config from '../assets/config/config.json';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DocViewComponent } from './doc-view/doc-view.component';
 import { ScanQrCodeComponent } from './scan-qr-code/scan-qr-code.component';
 
-let configData = {
-  baseUrl: `${config.bffUrl}/v1/credentials`
+import ISO6391 from 'iso-639-1';
+import * as config from '.././assets/config/config.json';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
   declarations: [
     AppComponent,
-    // VerifyCertificateComponent,
     ScanQrCodeComponent,
     DocViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // VerifyModule.forChild(configData),
-    // ZXingScannerModule,
     QuarModule,
     HttpClientModule,
     NgxExtendedPdfViewerModule,
+    TranslateModule.forRoot({
+      loader: { provide: TranslateLoader, useFactory: (createTranslateLoader), deps: [HttpClient] }
+    }),
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  languages;
+  constructor(translate: TranslateService) {
+
+    translate.setDefaultLang('en');
+    this.languages = config.languages;
+    var installed_languages = [];
+
+    for (let i = 0; i < this.languages.length; i++) {
+      installed_languages.push({
+        "code": this.languages[i],
+        "name": ISO6391.getNativeName(this.languages[i])
+      });
+    }
+
+    localStorage.setItem('languages', JSON.stringify(installed_languages));
+    translate.addLangs(this.languages);
+
+    if (localStorage.getItem('setLanguage') && this.languages.includes(localStorage.getItem('setLanguage'))) {
+      translate.use(localStorage.getItem('setLanguage'));
+
+    } else {
+      const browserLang = translate.getBrowserLang();
+      let lang = this.languages.includes(browserLang) ? browserLang : 'en';
+      translate.use(lang);
+      localStorage.setItem('setLanguage', lang);
+    }
+  }
+}
