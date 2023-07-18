017f9e10e1b3de0d893d859119c66daee1959bb6
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-07-18 16:17:49 +0530

    Issue #ULP-000 feat: Verification through route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,31 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
 
+import { QuarModule } from '@altack/quar';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import * as config from '../assets/config/config.json';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VerifyModule } from 'vc-verification';
-import { ZXingScannerModule } from '@zxing/ngx-scanner';
-import { VerifyCertificateComponent } from './verify-certificate/verify-certificate.component';
-import { environment } from '../environments/environment';
-import { AppConfig } from './app.config';
-import { HttpClientModule } from '@angular/common/http';
-import * as config from '../assets/config/config.json';
-import { ScanQrCodeComponent } from './scan-qr-code/scan-qr-code.component';
-import { QuarModule } from '@altack/quar';
 import { DocViewComponent } from './doc-view/doc-view.component';
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
-
-
+import { ScanQrCodeComponent } from './scan-qr-code/scan-qr-code.component';
 
 let configData = {
   baseUrl: `${config.bffUrl}/v1/credentials`
 }
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
-    VerifyCertificateComponent,
+    // VerifyCertificateComponent,
     ScanQrCodeComponent,
     DocViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    VerifyModule.forChild(configData),
-    ZXingScannerModule,
+    // VerifyModule.forChild(configData),
+    // ZXingScannerModule,
     QuarModule,
     HttpClientModule,
     NgxExtendedPdfViewerModule,
--- src/app/scan-qr-code/scan-qr-code.component.ts
+++ src/app/scan-qr-code/scan-qr-code.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import * as JSZip from 'jszip';
 import { concatMap, map } from 'rxjs/operators';
 import { QuarComponent } from '@altack/quar';
 import { DataService } from '../services/data.service';
 import { CredentialService } from '../services/credential/credential.service';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-scan-qr-code',
@@ -15,14 +15,14 @@ export class ScanQrCodeComponent implements OnInit {
   loader: boolean = false;
   invalidQRCode: boolean = false;
   qrString: string;
-  entity: any;
-  model;
-  isScanCompleted = false;
-  scanQrCode = true
-  verified = false
-  expired = false
-  notVerified = false
-  credential: any
+  isScanCompleted = true;
+  scanQrCode = true;
+  verified = false;
+  expired = false;
+  notVerified = false;
+  credential: any;
+  credentialId: string;
+
   @ViewChild(QuarComponent) private quar: QuarComponent;
   constructor(
     private readonly route: ActivatedRoute,
@@ -33,15 +33,26 @@ export class ScanQrCodeComponent implements OnInit {
 
   ngOnInit(): void {
     this.scanQrCode = true
-    this.isScanCompleted = true;
-    this.verified = false
-    this.expired = false
-    this.notVerified = false
+    this.verified = false;
+    this.expired = false;
+    this.notVerified = false;
     this.route.params.subscribe(params => {
-      console.log(params);
+      console.log("params", params);
 
-      if (params['entity']) {
-        this.entity = params['entity'];
+      if (params['credentialId']) {
+        this.credentialId = params['credentialId'];
+
+        if (this.credentialId) {
+          this.loader = true;
+          this.isScanCompleted = true;
+          this.scanQrCode = false;
+          this.verifyCredential(this.credentialId);
+          this.fetchCredential(this.credentialId);
+        } else {
+          this.loader = false;
+          this.invalidQRCode = true;
+          this.restartScanning();
+        }
       }
     })
   }
@@ -52,26 +63,6 @@ export class ScanQrCodeComponent implements OnInit {
     this.isScanCompleted = true;
     console.log("event", event);
     if (event) {
-
-      // this.dataService.verify(event).subscribe((res: any) => {
-      //   console.log(res)
-      //   console.log(res.checks[0].expired)
-      //   if (res.checks[0].expired == 'OK') {
-      //     this.loader = false
-      //     this.verified = true
-      //   } else {
-      //     this.loader = false
-      //     this.verified = true
-      //     this.expired = true
-      //   }
-
-      // }, error => {
-      //   this.loader = false
-      //   this.invalidQRCode = true
-      //   this.notVerified = true
-      //   console.log(error)
-      // })
-
       try {
         let url: string;
         let credentialId;
@@ -82,54 +73,9 @@ export class ScanQrCodeComponent implements OnInit {
           console.log("CredentialId", credentialId);
         }
 
-        let credential;
         if (credentialId) {
-          this.dataService.verify(credentialId).subscribe((res: any) => {
-            console.log(res)
-            console.log(res.result.checks[0].expired)
-            if (res.result.checks[0].expired == 'OK') {
-              this.loader = false;
-              this.verified = true;
-            } else {
-              this.loader = false;
-              this.verified = true;
-              this.expired = true;
-            }
-
-          }, error => {
-            this.loader = false;
-            //this.invalidQRCode = true
-            this.notVerified = true;
-            console.log(error);
-          });
-
-          this.credentialService.getToken().pipe(concatMap(_ =>
-            this.credentialService.getCredentialById(credentialId).pipe(map(res => {
-              credential = res;
-              console.log("res", res);
-              return res;
-            }))),
-            concatMap(_ => this.credentialService.getCredentialSchemaId(credentialId)),
-            concatMap((res: any) => {
-              console.log("res", res);
-              credential.schemaId = res.credential_schema;
-              return this.credentialService.getSchema(res.credential_schema).pipe(
-                map((schema: any) => {
-                  credential.credential_schema = schema;
-                  return credential;
-                })
-              );
-            })
-          ).subscribe((res: any) => {
-            console.log(res)
-            //this.loader = false;
-            this.credential = credential
-          }, (error: any) => {
-            this.loader = false;
-            this.notVerified = true
-            this.restartScanning();
-          });
-
+          this.verifyCredential(credentialId);
+          this.fetchCredential(credentialId);
         } else {
           this.loader = false;
           // this.notVerified = true;
@@ -142,6 +88,65 @@ export class ScanQrCodeComponent implements OnInit {
         this.restartScanning();
       }
     }
+  }
+
+  verifyCredential(credentialId: string) {
+    this.dataService.verify(credentialId)
+      .pipe(map(res => {
+        if (res?.result) {
+          if (res?.result?.errors) {
+            throw new Error('Error occurred!')
+          }
+          return res;
+        } else {
+          throw new Error('Error occurred!');
+        }
+      }))
+      .subscribe((res: any) => {
+        console.log(res)
+        console.log(res?.result?.checks?.[0]?.expired)
+        if (res?.result?.checks?.[0]?.expired === 'OK') {
+          this.loader = false;
+          this.verified = true;
+        } else {
+          this.loader = false;
+          this.verified = true;
+          this.expired = true;
+        }
+      }, error => {
+        this.loader = false;
+        this.notVerified = true;
+      });
+  }
+
+  fetchCredential(credentialId: string) {
+    let credential;
+    this.credentialService.getToken().pipe(concatMap(_ =>
+      this.credentialService.getCredentialById(credentialId).pipe(map(res => {
+        credential = res;
+        console.log("res", res);
+        return res;
+      }))),
+      concatMap(_ => this.credentialService.getCredentialSchemaId(credentialId)),
+      concatMap((res: any) => {
+        console.log("res", res);
+        credential.schemaId = res.credential_schema;
+        return this.credentialService.getSchema(res.credential_schema).pipe(
+          map((schema: any) => {
+            credential.credential_schema = schema;
+            return credential;
+          })
+        );
+      })
+    ).subscribe((res: any) => {
+      console.log(res)
+      this.loader = false;
+      this.credential = credential
+    }, (error: any) => {
+      this.loader = false;
+      // this.notVerified = true;
+      this.restartScanning();
+    });
   }
 
   restartScanning() {
@@ -166,8 +171,8 @@ export class ScanQrCodeComponent implements OnInit {
 
   scan() {
     console.log("scan")
-    this.scanQrCode = false
-    this.isScanCompleted = false
+    this.scanQrCode = false;
+    this.isScanCompleted = false;
   }
 
   back() {
@@ -177,6 +182,4 @@ export class ScanQrCodeComponent implements OnInit {
     this.notVerified = false;
     this.expired = false;
   }
-
-
 }
